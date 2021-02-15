0047498119fdc672694dfc54af92c96d06824de5
Egor Romanov <unsaidxpl@gmail.com>
2021-02-15 23:23:38 +0300

    Generate GraphQL

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
+  post "/graphql", to: "graphql#execute"
+
   resource :users, only: [:create]
 
   get '/sign-in', to: 'users#sign_in'
