cba5b372d5f6a9b3dfec360542992ea57bdecddf
Egor Romanov <unsaidxpl@gmail.com>
2021-02-19 23:10:20 +0300

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,24 @@
-User.create(
-  [
-    {
-      username: 'admin',
-      email: 'admin@mail.com',
-      password: ENV.fetch('ADMIN_PASSWORD'),
-      full_name: 'Admin',
-    },
-    {
-      username: 'tester',
-      email: 'test@user.ru',
-      password: 'test123',
-      full_name: 'Tester',
-    }
-  ]
+admin_user = User.create(
+  username: 'admin',
+  email: 'admin@mail.com',
+  password: ENV.fetch('ADMIN_PASSWORD'),
+  full_name: 'Admin',
+)
+tester_user = User.create(
+  username: 'tester',
+  email: 'test@user.ru',
+  password: 'test123',
+  full_name: 'Tester',
 )
 
-Post.create(
+posts = Post.create(
   [
     {
-      user: User.first,
+      user_id: admin_user.id,
       body: Faker::ChuckNorris.fact,
     },
     {
-      user: User.first,
+      user_id: admin_user.id,
       body: Faker::Lorem.sentence,
     }
   ]
@@ -31,14 +27,14 @@ Post.create(
 Comment.create(
   [
     {
-      post: Post.first.id,
+      post_id: posts.first.id,
       body: Faker::Hipster.sentence,
-      user: User.last,
+      user_id: tester_user.id,
     },
     {
-      post: Post.first.id,
+      post_id: posts.second.id,
       body: Faker::Restaurant.description,
-      user: User.last,
+      user_id: tester_user.id,
     }
   ]
 )
