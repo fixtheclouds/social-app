f9abd75290caeb3e1c8cf35823b05bec1d93553e
Egor Romanov <unsaidxpl@gmail.com>
2021-02-19 23:04:32 +0300

    Add comment type

--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -1,14 +1,11 @@
 module Types
-  class PostType < Types::BaseObject
+  class CommentType < Types::BaseObject
     field :id, ID, null: false
-    field :title, String, null: true
     field :body, String, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
-
-    # Relations
     field :user, Types::UserType, null: false
-    field :parent, Types::PostType, null: true
-    field :comments, [Types::CommentType], null: true
+    field :post, Types::PostType, null: false
+    field :parent, Types::CommentType, null: true
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
 User.create(
-  username: 'admin',
-  email: 'admin@mail.com',
-  password: ENV.fetch('ADMIN_PASSWORD'),
-  full_name: 'Admin'
+  [
+    {
+      username: 'admin',
+      email: 'admin@mail.com',
+      password: ENV.fetch('ADMIN_PASSWORD'),
+      full_name: 'Admin',
+    },
+    {
+      username: 'tester',
+      email: 'test@user.ru',
+      password: 'test123',
+      full_name: 'Tester',
+    }
+  ]
 )
 
 Post.create(
@@ -13,7 +23,22 @@ Post.create(
     },
     {
       user: User.first,
-      body: Faker::Lorem.sentence
+      body: Faker::Lorem.sentence,
+    }
+  ]
+)
+
+Comment.create(
+  [
+    {
+      post: Post.first.id,
+      body: Faker::Hipster.sentence,
+      user: User.last,
+    },
+    {
+      post: Post.first.id,
+      body: Faker::Restaurant.description,
+      user: User.last,
     }
   ]
 )
