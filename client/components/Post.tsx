04bc49b515a083dccb8e1ea5d759cc30da2a4c12
Egor Romanov <unsaidxpl@gmail.com>
2021-04-05 23:36:59 +0300

    Try to organize components

---
+++ client/components/Post.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { Box } from "@chakra-ui/react"
+
+export default function Post() {
+  return (
+    <Box bg="lavender" w="200" h="200">
+      Test box
+    </Box>
+  )
+}
