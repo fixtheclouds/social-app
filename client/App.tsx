d4fa3d27a0ddc230688b75b69a4d65ff960e37ff
Egor Romanov <unsaidxpl@gmail.com>
2021-04-05 22:44:44 +0300

    Add other apollo & gql modules

--- client/App.tsx
+++ client/App.tsx
@@ -15,18 +15,7 @@ export const App = () => (
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <VStack spacing={8}>
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+          Introducing
         </VStack>
       </Grid>
     </Box>
