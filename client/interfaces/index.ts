552e6d2425f22bad4f7deb4f33bf37d9e3a8b4ce
Egor Romanov <unsaidxpl@gmail.com>
2021-04-11 21:27:24 +0300

    Update interfaces a bit

--- client/interfaces/index.ts
+++ client/interfaces/index.ts
@@ -2,6 +2,7 @@ export interface IUser {
   id: number
   username: string
   fullName: string
+  lastSignInAt?: number
 }
 
 export interface IPost {
@@ -9,5 +10,6 @@ export interface IPost {
   title: string
   body: string
   createdAt: number
+  updatedAt: number
   user: IUser
 }
