1e345b51ed9e7ec3e49d6bef4963c0744307be90
Egor Romanov <egor@republic.co>
2022-02-03 13:49:14 +0300

    Some setup fixes

--- docker/setup.sh
+++ docker/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Create .env and fill missing values
-ADMIN_PASSWORD = $(cat /dev/urandom | tr -dc a-zA-Z0-9 | fold -w 14 | head -n 1)
+ADMIN_PASSWORD=$(cat /dev/urandom | tr -dc a-zA-Z0-9 | fold -w 14 | head -n 1)
 cp .env.example .env
 echo "ADMIN_PASSWORD=${ADMIN_PASSWORD}" >> .env
 
